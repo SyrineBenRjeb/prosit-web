1e2bae8bb4e396ccf475c6ac6c16cb7ffa5d6a08
SyrineBenRejeb <syrinebenrejeb49@gmail.com>
2025-10-02 12:52:16 +0100

    sceance 4

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,12 +16,20 @@ final class AuthorController extends AbstractController
             'controller_name' => 'AuthorController',
         ]);
     }
-    #[Route( '/show/{name}/{prenom}',name: 'showAuthor')]
+    #[Route( '/show/{name}_{prenom}',name: 'showAuthor')]
 public function showAuthor(string $name, string $prenom):Response{
        return $this->render('author/show.html.twig' , [
         'nom' => $name, 
         'prenom' => $prenom
     ]);
 
+
     }
+    #[Route('/ShowAll' ,name:'ShowAll')]
+   public function ShowAll(AuthorRepository $repo):Response{
+    $authors=$repo->findAll();
+    return $this->render('author/ShowAll.html.twig', ['authors' => $authors]);
+   }
+
+
 }
